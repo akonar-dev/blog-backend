fd18bf04a823533e11e7ef820d04d7bf9281e3ed
akonar-dev <157737602+akonar-dev@users.noreply.github.com>
2024-07-20 15:59:56 +0530

    Add environment variable

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my blog website')
-})
+let PORT = process.env.PORT || 3000;
 
-app.listen(3000,() => {
-    console.log('listening on port 3000')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to my blog website");
+});
+
+app.listen(PORT, () => {
+  console.log("listening on port 3000");
+});
